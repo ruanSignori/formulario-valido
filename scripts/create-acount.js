2759e6fdbc25f9278d1d0f9d78cb9cc77d68ba6c
ruanSignori <86364910+ruanSignori@users.noreply.github.com>
2021-09-07 08:06:13 -0300

    master

--- scripts/create-acount.js
+++ scripts/create-acount.js
@@ -1,9 +1,4 @@
 let cpf = document.querySelector('input.cpf');
-console.log(cpf)
-
-if(cpf.length === 3) {
-    cpf.style.color = 'red';
-}
 
 class FormValidation{
     constructor (){
@@ -12,9 +7,11 @@ class FormValidation{
         this.events();
     }
 
+    //Capturar o evento de tentativa de validação do formulário
     events() {
         this.form.addEventListener('submit', e => {
             this.handleSubmit(e)
+            this.validFields()
         });
     }
 
@@ -22,6 +19,32 @@ class FormValidation{
         e.preventDefault();
     }
 
+    validFields() {
+        let valid = true;
+
+        for (let errorTxt of this.form.querySelectorAll('.text-error')){
+            errorTxt.remove();
+
+            for (let field of this.form.querySelectorAll('.valid')){
+                const placeholder = field.previousElementSibling.innerText;
+
+                if(!field.value) {
+                    this.error(field, `Campo ${placeholder} não pode estar vazio`)
+                    console.log('ERROR')
+                }
+            }
+        }
+    }
+
+    //Criar uma div embaixo daonde ocorreu o erro
+    error (field, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('text-error');
+        field.insertAdjacentElement('afterend', div);
+    }
+
 }
 
 const validated = new FormValidation();
+console.log(validated)
